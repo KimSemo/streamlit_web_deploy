f720649d494c178de15f2f7825856a47f041dad7
unknown <san6659@gmail.com>
2023-05-28 23:27:03 +0900

    Create Python Streamlit Project

---
+++ pages/1_Live_dataImport.py
@@ -0,0 +1,373 @@
+# 깃허브를 통해 실시간 데이터 받는 코드!
+
+# import streamlit as st
+# import numpy as np
+# import pandas as pd
+# import requests
+# import matplotlib.pyplot as plt
+# import time
+
+
+
+# # 데이터 가져오기
+# def get_realtime_data():
+#     url = f"https://raw.githubusercontent.com/changyeon99/TimingBeltData/main/A2023-05-28 00:03.txt"
+#     response = requests.get(url)
+#     if response.status_code == 200:
+#         data = response.text
+#         return data
+#     else:
+#         return None
+
+# # 스트림릿 앱 시작
+# st.header("Real-time Data Visualization")
+
+# # 데이터 처리 및 시각화
+# while True:
+#     data = get_realtime_data()
+#     if data:
+#         # 데이터 전처리
+#         lines = data.strip().split("\n")
+#         values = [float(line) for line in lines]
+
+#         # 데이터프레임 생성
+#         df = pd.DataFrame({"Value": values})
+
+#         # 그래프 생성
+#         fig, ax = plt.subplots(figsize=(12, 4))
+#         ax.plot(df.index, df["Value"])
+#         ax.set_xlabel("Time")
+#         ax.set_ylabel("Value")
+#         ax.set_title("Real-time Data")
+#         st.pyplot(fig)
+
+#     # 1초마다 데이터 갱신
+#     time.sleep(1)
+
+
+## 위에가 original
+
+
+
+# ######잘 돌아가는 코드 근데 파일갱신이 필요
+# import streamlit as st
+# import numpy as np
+# import pandas as pd
+# import requests
+# import matplotlib.pyplot as plt
+# import time
+
+# # 데이터 가져오기
+# def get_realtime_data():
+#     url = "https://raw.githubusercontent.com/changyeon99/TimingBeltData/main/A2023-05-28%2000:03.txt"
+#     response = requests.get(url)
+#     if response.status_code == 200:
+#         data = response.text
+#         return data
+#     else:
+#         return None
+
+# # 스트림릿 앱 시작
+# st.header("Real-time Data Visualization")
+
+# # 데이터 처리 및 시각화
+# while True:
+#     data = get_realtime_data()
+#     if data:
+#         # 데이터 전처리
+#         lines = data.strip().split("\n")
+#         values = []
+#         for line in lines:
+#             if line:
+#                 try:
+#                     parts = line.split(",")
+#                     value = float(parts[0])  # 센서값이 두 번째 열에 위치하는 것으로 가정
+#                     values.append(value)
+#                 except ValueError:
+#                     continue
+
+#         # 데이터프레임 생성
+#         df = pd.DataFrame({"Value": values})
+
+#         # 그래프 생성
+#         fig, ax = plt.subplots(figsize=(12, 4))
+#         ax.plot(df.index, df["Value"])
+#         ax.set_xlabel("Time")
+#         ax.set_ylabel("Value")
+#         ax.set_title("Real-time Data")
+#         st.pyplot(fig)
+
+#     # 1초마다 데이터 갱신
+#     time.sleep(1)
+
+
+########위에 잘 돌아가는 코드 수정해봄(데이터 파일 갱신되게)
+
+## start시간 파일만 맞으면 끝나는 시간을 엄청 뒤로해두면 실시간으로 임포트한 데이터를 볼 수 있다.
+# import streamlit as st
+# import numpy as np
+# import pandas as pd
+# import requests
+# import matplotlib.pyplot as plt
+# import time
+
+# # 데이터 가져오기
+# def get_realtime_data(file_name):
+#     base_url = "https://raw.githubusercontent.com/changyeon99/TimingBeltData/main/"
+#     url = base_url + file_name
+#     response = requests.get(url)
+#     if response.status_code == 200:
+#         data = response.text
+#         return data
+#     else:
+#         return None
+# df = pd.read_txt(file_list[0], sep='\t',header=None)
+# # 스트림릿 앱 시작
+# st.header("Real-time Data Visualization")
+
+# # 데이터 처리 및 시각화
+# start_time = time.strptime("2023-05-28 00:03", "%Y-%m-%d %H:%M")
+# end_time = time.strptime("2023-05-28 11:28", "%Y-%m-%d %H:%M")       ##10:46
+# current_time = start_time
+
+# while current_time <= end_time:
+#     file_name = time.strftime("A%Y-%m-%d %H:%M.txt", current_time)
+#     data = get_realtime_data(file_name)
+#     if data:
+#         # 데이터 전처리 및 시각화 작업 수행
+#         lines = data.strip().split("\n")
+#         values = []
+#         for line in lines:
+#             if line:
+#                 try:
+#                     value = float(line.split(",")[0])
+#                     values.append(value)
+#                 except ValueError:
+#                     continue
+
+#         if values:
+#             df = pd.DataFrame({"Value": values})
+
+#             fig, ax = plt.subplots(figsize=(12, 4))
+#             ax.plot(df.index, df["Value"])
+#             ax.set_xlabel("Time")
+#             ax.set_ylabel("Value")
+#             ax.set_title(f"Real-time Data - {time.strftime('%Y-%m-%d %H:%M', current_time)}")
+#             st.pyplot(fig)
+
+#     # 다음 시간으로 업데이트
+#     current_time = time.localtime(time.mktime(current_time) + 60)  # 60 seconds = 1 minute
+
+#     # 1초마다 데이터 갱신
+#     time.sleep(1)
+
+
+# import streamlit as st
+# import numpy as np
+# import pandas as pd
+# import requests
+# import matplotlib.pyplot as plt
+# import time
+
+# # 데이터 가져오기
+# def get_realtime_data(file_name):
+#     base_url = "https://raw.githubusercontent.com/changyeon99/TimingBeltData/main/"
+#     url = base_url + file_name
+#     response = requests.get(url)
+#     if response.status_code == 200:
+#         data = response.text
+#         return data
+#     else:
+#         return None
+
+# # 스트림릿 앱 시작
+# st.header("Real-time Data Visualization")
+
+# # 데이터 처리 및 시각화
+# start_time = time.strptime("2023-05-28 00:03", "%Y-%m-%d %H:%M")
+# end_time = time.strptime("2023-05-28 10:46", "%Y-%m-%d %H:%M")
+# current_time = start_time
+
+# while current_time <= end_time:
+#     file_name = time.strftime("A%Y-%m-%d %H:%M.txt", current_time)
+#     data = get_realtime_data(file_name)
+#     if data:
+#         # 데이터 전처리
+#         lines = data.strip().split("\n")
+#         values = [float(line.split(",")[0]) for line in lines if line]
+
+#         # 데이터프레임 생성
+#         df = pd.DataFrame({"Value": values})
+
+#         # 그래프 생성
+#         fig, ax = plt.subplots(figsize=(12, 4))
+#         ax.plot(df.index, df["Value"])
+#         ax.set_xlabel("Time")
+#         ax.set_ylabel("Value")
+#         ax.set_title(f"Real-time Data - {time.strftime('%Y-%m-%d %H:%M', current_time)}")
+#         st.pyplot(fig)
+
+#         # 데이터 출력
+#         st.dataframe(df)
+
+#     # 다음 시간으로 업데이트
+#     current_time = time.localtime(time.mktime(current_time) + 60)  # 60 seconds = 1 minute
+
+#     # 1초마다 데이터 갱신
+#     time.sleep(1)
+
+
+
+
+################## 최종 완료 코드 ###################
+# import streamlit as st
+# import numpy as np
+# import pandas as pd
+# import requests
+# import matplotlib.pyplot as plt
+# import time
+
+# # 데이터 가져오기
+# def get_realtime_data(file_name):
+#     base_url = "https://raw.githubusercontent.com/changyeon99/TimingBeltData/main/"
+#     url = base_url + file_name
+#     response = requests.get(url)
+#     if response.status_code == 200:
+#         data = response.text
+#         return data
+#     else:
+#         return None
+
+# # 스트림릿 앱 시작
+# st.header("Real-time Data Visualization")
+
+# # 데이터 처리 및 시각화
+# start_time = time.strptime("2023-05-28 00:03", "%Y-%m-%d %H:%M")
+# end_time = time.strptime("2023-05-28 10:46", "%Y-%m-%d %H:%M")
+# current_time = start_time
+
+# while current_time <= end_time:
+#     file_name = time.strftime("A%Y-%m-%d %H:%M.txt", current_time)
+#     data = get_realtime_data(file_name)
+#     if data:
+#         # 데이터 전처리
+#         lines = data.strip().split("\n")
+#         values_1 = []
+#         values_2 = []
+#         values_3 = []
+#         for line in lines:
+#             if line:
+#                 parts = line.split(",")
+#                 if len(parts) >= 3:
+#                     try:
+#                         value_1 = float(parts[0])
+#                         value_2 = float(parts[1])
+#                         value_3 = float(parts[2])
+#                         values_1.append(value_1)
+#                         values_2.append(value_2)
+#                         values_3.append(value_3)
+#                     except ValueError:
+#                         continue
+
+#         # 데이터프레임 생성
+#         df = pd.DataFrame({"Value 1": values_1, "Value 2": values_2, "Value 3": values_3})
+
+#         # 그래프 생성
+#         fig, ax = plt.subplots(figsize=(12, 4))
+#         ax.plot(df.index, df["Value 1"], label="Value 1")
+#         ax.plot(df.index, df["Value 2"], label="Value 2")
+#         ax.plot(df.index, df["Value 3"], label="Value 3")
+#         ax.set_xlabel("Time")
+#         ax.set_ylabel("Value")
+#         ax.set_title(f"Real-time Data - {time.strftime('%Y-%m-%d %H:%M', current_time)}")
+#         ax.legend()
+#         st.pyplot(fig)
+
+#         # 데이터 출력
+#         st.dataframe(df)
+
+#     # 다음 시간으로 업데이트
+#     current_time = time.localtime(time.mktime(current_time) + 60)  # 60 seconds = 1 minute
+
+#     # 1초마다 데이터 갱신
+#     time.sleep(1)
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+import requests
+import matplotlib.pyplot as plt
+import time
+
+# 데이터 가져오기
+def get_realtime_data(file_name):
+    base_url = "https://raw.githubusercontent.com/changyeon99/TimingBeltData/main/"
+    url = base_url + file_name
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.text
+        return data
+    else:
+        return None
+
+# 스트림릿 앱 시작
+st.header("Real-time Data Visualization")
+
+# 데이터 처리 및 시각화
+start_time = time.strptime("2023-05-28 00:03", "%Y-%m-%d %H:%M")
+end_time = time.strptime("2023-05-28 10:46", "%Y-%m-%d %H:%M")
+current_time = start_time
+
+while current_time <= end_time:
+    file_name = time.strftime("A%Y-%m-%d %H:%M.txt", current_time)
+    data = get_realtime_data(file_name)
+    if data:
+        # 데이터 전처리
+        lines = data.strip().split("\n")
+        values_1 = []
+        values_2 = []
+        values_3 = []
+        for line in lines:
+            if line:
+                parts = line.split(",")
+                if len(parts) >= 3:
+                    try:
+                        value_1 = float(parts[0])
+                        value_2 = float(parts[1])
+                        value_3 = float(parts[2])
+                        values_1.append(value_1)
+                        values_2.append(value_2)
+                        values_3.append(value_3)
+                    except ValueError:
+                        continue
+
+        # 데이터프레임 생성
+        df = pd.DataFrame({"Value 1": values_1, "Value 2": values_2, "Value 3": values_3})
+
+        # 그래프 생성
+        fig, ax = plt.subplots(figsize=(12, 4))
+        ax.plot(df.index, df["Value 1"], label="Value 1")
+        ax.plot(df.index, df["Value 2"], label="Value 2")
+        ax.plot(df.index, df["Value 3"], label="Value 3")
+        ax.set_xlabel("Time")
+        ax.set_ylabel("Value")
+        ax.set_title(f"Real-time Data - {time.strftime('%Y-%m-%d %H:%M', current_time)}")
+        ax.legend()
+        st.pyplot(fig)
+
+        # 그래프 삭제
+        plt.close(fig)
+
+        # 데이터 출력
+        st.dataframe(df)
+
+    # 다음 시간으로 업데이트
+    current_time = time.localtime(time.mktime(current_time) + 60)  # 60 seconds = 1 minute
+
+    # 1초마다 데이터 갱신
+    time.sleep(1)
+
+
+
+
+
--- pages/NASA/3_🛹_signalProcessing2.py
+++ pages/NASA/3_🛹_signalProcessing2.py
@@ -55,67 +55,99 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import glob
-import os, sys
+import requests
 import matplotlib.pyplot as plt
-import matplotlib
-matplotlib.use('agg')
 from scipy.fft import fft
+import time
 
-os.chdir("C:\\Users\\San\\Desktop\\23-1\\cap\\practice\\샘플 데이터\\NASA Bearing Dataset\\2nd_test")
-file_list = glob.glob("**/*.39", recursive=True)
+# 데이터 가져오기
+def get_realtime_data(file_name):
+    base_url = "https://raw.githubusercontent.com/changyeon99/TimingBeltData/main/"
+    url = base_url + file_name
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.text
+        return data
+    else:
+        return None
 
-df = pd.read_csv(file_list[0], sep='\t', header=None)
+# RMS 신호처리 함수
+def rms(stats):
+    return np.sqrt(np.mean(stats**2, axis=0))
 
-# FFT 신호 처리 함수
+# FFT 신호처리 함수
 def fft_signal(stats):
     return np.abs(fft(stats, axis=0))
 
-# 2000개의 데이터 샘플을 하나의 파일로 추출
-fft_arr = []
-for file in file_list:
-    df = pd.read_csv(file, sep='\t', header=None)
-    fft_value = fft_signal(df.values)
-    fft_arr.append(fft_value)
-fft_arr = np.concatenate(fft_arr, axis=0)
+# 스트림릿 앱 시작
+st.header("Real-time Data Visualization")
+st.subheader("Select Signal Processing Technique")
 
-fft_df = pd.DataFrame(fft_arr, columns=['ch1', 'ch2', 'ch3', 'ch4'])
-fft_df.to_csv('C:\\Users\\San\\Desktop\\23-1\\cap\\practice\\샘플 데이터\\NASA Bearing Dataset\\FFT_bearing.csv', index=None)
+# 신호처리 기법 선택
+processing_technique = st.selectbox("Processing Technique", ["RMS", "FFT"])
 
-# 스트림릿 앱의 헤더 및 설명 추가
-st.header("FFT 신호 처리")
-st.write("FFT를 사용하여 신호를 처리하고 시각화합니다.")
+# 데이터 처리 및 시각화
+start_time = time.strptime("2023-05-28 00:03", "%Y-%m-%d %H:%M")
+end_time = time.strptime("2023-05-28 10:46", "%Y-%m-%d %H:%M")
+current_time = start_time
+
+while current_time <= end_time:
+    file_name = time.strftime("A%Y-%m-%d %H:%M.txt", current_time)
+    data = get_realtime_data(file_name)
+    if data:
+        # 데이터 전처리
+        lines = data.strip().split("\n")
+        values_1 = []
+        values_2 = []
+        values_3 = []
+
+        for line in lines:
+            parts = line.strip().split(",")
+            if len(parts) >= 3:
+                if parts[0] != '' and parts[1] != '' and parts[2] != '':
+                    try:
+                        value_1 = float(parts[0])
+                        value_2 = float(parts[1])
+                        value_3 = float(parts[2])
+                        values_1.append(value_1)
+                        values_2.append(value_2)
+                        values_3.append(value_3)
+                    except ValueError:
+                        # 부동 소수점으로 변환할 수 없는 문자열이 있는 경우 건너뜁니다.
+                        continue
+
+        df = pd.DataFrame({"ch1": values_1, "ch2": values_2, "ch3": values_3})
+
+        # 신호처리 기법 선택에 따른 처리
+        if processing_technique == "RMS":
+            processed_values = rms(df.values)
+            processed_df = pd.DataFrame(processed_values.reshape(1, -1), columns=[f"RMS Value {i+1}" for i in range(processed_values.shape[0])])
+        else:
+            processed_values = fft_signal(df.values)
+            processed_df = pd.DataFrame(processed_values.T, columns=["FFT Value 1", "FFT Value 2", "FFT Value 3"])
+
+        # 그래프 출력
+        fig, ax = plt.subplots(figsize=(12, 4))
+        ax.plot(processed_df.values.flatten())
+        ax.legend(processed_df.columns)
+        ax.set_xlabel("Time")
+        ax.set_ylabel("Value")
+        ax.set_title(f"{processing_technique} Data - {time.strftime('%Y-%m-%d %H:%M', current_time)}")
+        st.pyplot(fig)
+
+        # 데이터 출력
+        st.dataframe(processed_df)
+
+    # 다음 시간으로 업데이트
+    current_time = time.localtime(time.mktime(current_time) + 60)  # 60 seconds = 1 minute
+
+    # 그래프 닫기
+    plt.close()
+
+    # 1초마다 데이터 갱신
+    time.sleep(1)
 
 
-# Matplotlib figure 생성
-with st.spinner('로딩 중...'):
-    fig, ax = plt.subplots(figsize=(12, 8), tight_layout=True)
-
-    # 전체 그래프 데이터
-    all_signals = []
-
-    # 개별 그래프 플롯
-    for i in range(4):
-        signal = fft_df[f'ch{i+1}'].values
-
-        # 선 그래프 플롯
-        ax.plot(signal, label=f'Ch{i+1} FFT')
-
-        # 전체 그래프 데이터에 추가
-        all_signals.extend(signal)
-
-    # 전체 그래프 플롯
-    ax.plot(all_signals, label='Combined FFT', color='black', linewidth=2)
-
-    # 범례와 제목 추가
-    ax.legend()
-    ax.set_title('Combined FFT')
-    ax.set_xlabel('Frequency')
-    ax.set_ylabel('Amplitude')
-
-    # 그래프 표시
-    #fig = st.line_chart()
-    st.pyplot(fig)
 
 
 
